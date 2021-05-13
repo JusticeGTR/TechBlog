6403889b5122cfa4a52ba6bdb0ecc3ee17be1674
Justin Lindsey <justinlindseylhr@gmail.com>
2021-05-13 09:50:07 -0500

    updating

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
+app.use(
+  session({
+    secret: "keyboard cat",
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+    resave: false,
+    proxy: true,
+  })
+);
 const PORT = process.env.PORT || 3001;
 
 // Create the Handlebars.js engine object with custom helper functions
