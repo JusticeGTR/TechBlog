cc7b776281b03e6692db1e702d5e177f60da2a01
Justin Lindsey <justinlindseylhr@gmail.com>
2021-05-13 20:08:30 -0500

    updating code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { User, Post } = require('../models')
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
